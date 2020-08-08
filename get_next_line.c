be4600bcdfabfbc15a07010de97b9bf465174224
lilangbr <eblang@gmail.com>
2020-08-08 18:11:10 -0300

    reorganizing

--- get_next_line.c
+++ get_next_line.c
@@ -22,90 +22,94 @@ static int ft_strlen(const char *s)
 		return (0);
 	return (ft_strlen(++s) + 1);
 }
-static int endofline(char *s)
+static int endofline(char *s) //Recebe o buffer
 {
 	int pos;
 
 	pos = -1;
-	if (!(*s))
+	if (!(*s)) //String nula "\o" NAO TEM \n
 		return (pos);
 	pos = 0;
-	while (s[pos] != '\0')
+	while (s[pos] != '\0') //Varre ate achar \n
 	{
 		if (s[pos] == '\n')
 			return (pos);
 		pos++;
 	}
-	if (pos == ft_strlen(s))
+	if (pos == ft_strlen(s)) //Percorreu sem encontrar
 		return (-2);
-	else
+	else //Nem eh nula, nem tem, nem n tem - portanto !s
 		return (-1);
 }
-static void ft_strappend(char **dst, char **add)
+static void ft_strappend(char **dst, char **add)// dst = s_main && add = buf
 {
 	size_t size;
 	size_t i;
 	size_t j;
 	char *aux;
-	char resto[10];
+	char resto[24];
 
 	*dst = malloc(1*sizeof(char)); //gnl
-	**dst = '\0'; 				//gnl
-	if (endofline(*add) < 0)
-		size = ft_strlen(*dst) + ft_strlen(*add) + 1;
+	**dst = '\0'; 				//gnl ..... EH A LINHA!
+	if (endofline(*add) < 0) //Nao tem \n no buffer
+		size = ft_strlen(*dst) + ft_strlen(*add) + 1; //pega todo o buffer
 	else
-		size = ft_strlen(*dst) + endofline(*add) + 1;
-	aux = malloc(size * sizeof(char));
+		size = ft_strlen(*dst) + endofline(*add) + 1; //pega ate \n
+	aux = malloc(size * sizeof(char)); //armazenamento temporario
     i = 0;
 	j = 0;
-	while (*dst[i])
+	while (*dst[i]) //se houver itens na linha, copia 
 	{
 		aux[i] = *dst[i];
 		i++;
 	}
-	while (**add != '\0' && **add != '\n')
+	while (**add != '\0' && **add != '\n') //copia buffer ate \n ou at o fim
 	{
 		aux[i] = **add;
 		i++;
 		j++;
 		(*add)++;
 	}
-	aux[i] = '\0';
-	free(*dst);
-	*dst = malloc(size*sizeof(char));
-	**dst = *aux;
-	(*dst)++;
-	aux++;
-	**dst = *aux;
-	(*dst)++;
-	aux++;
-	**dst = *aux;
-	aux--;
-	aux--;
-	free(aux);
-	(*dst)--;
-	(*dst)--;
-	resto[0] = (*add)[1];
-	resto[1] = (*add)[2];
-	resto[2] = (*add)[3];
-	resto[3] = (*add)[4];
-	resto[4] = (*add)[5];
-	resto[5] = (*add)[6];
-	resto[6] = '\0';
-	printf("%s\n", resto);
+	aux[i] = '\0'; 
+	(*add)++; //Atualiza posicao no buffer para a casa depois do \n
+	free(*dst); //libera linha anterior (pedaco anterior)
+	*dst = malloc(size*sizeof(char)); //aloca com o tamanho novo
+	//copia ate o final (\0)
+	i = 0;
+	while (aux[i]) //FAZER FUNCAO COPYSTR
+	{
+		(*dst)[i] = aux[i];
+		i++;
+	}
+	(*dst)[i] = '\0';
+	free(aux); //libera p n dar Mleak
+	//(*add)++; JA fiz na funcao append
+	i = 0;
+	while ((*add)[i]) //FAZER FUNCAO COPYSTR
+	{
+		resto[i] = (*add)[i];
+		i++;
+	}
+	resto[i] = '\0';
+	printf("----------------------------\n");
+	printf("REMANESCENTE: %s\n", resto);
+	printf("----------------------------\n");
 }
 int main()
 {
 	char **s_main;
 	char **buf;
-    char buffer[10] = "oi\nmenina";
+    char buffer[24] = "oi\nmenina\ntudo\nbem?";
 	char **final;
 
-	s_main = malloc(1*sizeof(char*));
-	buf = malloc(1*sizeof(char*));
-	*buf = buffer;
-	ft_strappend(s_main, buf);
+	printf("----------------------------\n");
+	printf("Conteudo buffer: 'oi\\nmenina,\\ntudo\\nbem?'\n");
+	s_main = malloc(1*sizeof(char*)); //pointer to pointer
+	buf = malloc(1*sizeof(char*)); //pointer to pointer
+	*buf = buffer; //pointer to string
+	ft_strappend(s_main, buf);//send pointers to pointers
 	final = s_main;
-	printf("%s\n", *final);
+	printf("LINHA: %s\n", *final);
+	printf("----------------------------\n");
 	return 0;
 }
