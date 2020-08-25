5178f1887e457767a0d4447e4feb244036a9a44d
lilangbr <eblang@gmail.com>
2020-08-25 19:40:12 -0300

    Reading without to test

--- get_next_line.c
+++ get_next_line.c
@@ -10,20 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h> //READ
+//#include <sys/stat.h>
+#include <sys/uio.h> //READ
+#include <fcntl.h> //OPEN
+#include <stdio.h> //PRINTF
+#include <stdlib.h> //MALLOC
+#include <unistd.h> //CLOSE
+#include <limits.h> //OPEN_MAX = 10240!
 
 static int ft_strlen(const char *s)
 {
 	if (!s)
-		return (0);
-	if (*s == '\0')
+		return (-1);
+	else if (*s == '\0')
 		return (0);
 	return (ft_strlen(++s) + 1);
 }
+/*
 static int endofline(char *s) //Recebe o buffer
-{
+{ //RETORNA POSICAO DO \n
 	int pos;
 
 	pos = -1;
@@ -41,75 +47,223 @@ static int endofline(char *s) //Recebe o buffer
 	else //Nem eh nula, nem tem, nem n tem - portanto !s
 		return (-1);
 }
-static void ft_strappend(char **dst, char **add)// dst = s_main && add = buf
-{
-	size_t size;
-	size_t i;
-	size_t j;
-	char *aux;
-	char resto[24];
+*/
+static void strcopy(char **dst, char **src, int start)
+{ //CONDICAO DST >= SRC!
+	int i;
+	int j;
 
-	*dst = malloc(1*sizeof(char)); //gnl
-	**dst = '\0'; 				//gnl ..... EH A LINHA!
-	if (endofline(*add) < 0) //Nao tem \n no buffer
-		size = ft_strlen(*dst) + ft_strlen(*add) + 1; //pega todo o buffer
-	else
-		size = ft_strlen(*dst) + endofline(*add) + 1; //pega ate \n
-	aux = malloc(size * sizeof(char)); //armazenamento temporario
-    i = 0;
-	j = 0;
-	while (*dst[i]) //se houver itens na linha, copia 
+	i = 0;
+	j = start;
+	while ((*src)[j]) //FAZER FUNCAO COPYSTR
 	{
-		aux[i] = *dst[i];
-		i++;
-	}
-	while (**add != '\0' && **add != '\n') //copia buffer ate \n ou at o fim
-	{
-		aux[i] = **add;
+		(*dst)[i] = (*src)[j];
+		//(*dst)++;//Incremento direto, sem i, j, pois qdo sai do escopo, zera.***
+		//(*src)++;
 		i++;
 		j++;
-		(*add)++;
-	}
-	aux[i] = '\0'; 
-	(*add)++; //Atualiza posicao no buffer para a casa depois do \n
-	free(*dst); //libera linha anterior (pedaco anterior)
-	*dst = malloc(size*sizeof(char)); //aloca com o tamanho novo
-	//copia ate o final (\0)
-	i = 0;
-	while (aux[i]) //FAZER FUNCAO COPYSTR
-	{
-		(*dst)[i] = aux[i];
-		i++;
 	}
 	(*dst)[i] = '\0';
-	free(aux); //libera p n dar Mleak
-	//(*add)++; JA fiz na funcao append
-	i = 0;
-	while ((*add)[i]) //FAZER FUNCAO COPYSTR
+}
+static void zerabuffer(char **buffer, int start)
+{
+	int i;
+
+	i = start;
+	while ((*buffer)[i])
 	{
-		resto[i] = (*add)[i];
+		(*buffer)[i] = '\0';
+		//(*buffer)++;
 		i++;
 	}
-	resto[i] = '\0';
-	printf("----------------------------\n");
-	printf("REMANESCENTE: %s\n", resto);
-	printf("----------------------------\n");
+}
+static int ft_strappend(char **dst, char **add)// dst = s_main && add = buf
+{   //ja existe dst!!!
+	//add nao tem /n!!
+	size_t size;
+	size_t i;//COMO MANDO **S, uso vars q mantenham as strs.***(anotar!)
+	size_t j;
+	char *aux;
+	
+	size = ft_strlen(*dst) + ft_strlen(*add) + 1; 
+	if (!(aux = malloc(size * sizeof(char)))) //VAI RECEBER A LINHA ANT + BUFF
+		return (1);
+    i = 0;
+	j = 0;
+	while ((*dst)[i]) //COPIA LINHA ANTERIOR EM AUX 
+	{
+		aux[i] = (*dst)[i];
+		i++;
+	}
+	free(*dst); //LIBERA LINHA (dst = line)
+	*dst = NULL;
+	while ((*add)[j] != '\0')
+	{
+		aux[i] = (*add)[j];//COPIA ANEXO EM AUX
+		i++;
+		j++;
+	}
+	aux[i] = '\0'; 
+	zerabuffer(add, 0);//LIBERA BUFF
+	//ATE P EFEITO DE CONTROLE(se esta zerado eh pq foi cpy)
+	if (!(*dst = malloc(size*sizeof(char)))) //aloca com o tamanho novo
+		return (1);
+	strcopy(dst, &aux, 0);	//COPIA em DTS, o SRC:
+	free(aux); //libera p n dar Mleak
+	aux = NULL;
+	return (0);
+}
+static int splitbuffer(char **buf, char **rest) //int = 0 nao tem resto
+{//FATIA BUF ATE PRIMEIRO \n - sem ele!. RESTANTE FICA EM REST - sem o 1o \n
+	int i;
+	int j;
+	int oneline;
+
+	i = 0;
+	j = 0;
+	while ((*buf)[i] != '\n' && (*buf)[i] != '\0')
+		i++;
+	if ((*buf)[i] == '\0')
+		return (0); //NAO \n, e portanto: NAO TEM RESTO
+	//ENTAO EH \n!
+	else if ((*buf)[i + 1] != '\0') //AINDA TEM CARACTERE NO BUFFER
+	{
+		strcopy(rest, buf, i + 1); //COPIA EM RESIDUAL apartir do \n
+		zerabuffer(buf, i); //vai zerar o inicio qdo copiar em *line!
+		//ja elimina o \n!!! (comeca a partir de i)*********
+	}
+	else //So tinha \n e nada mais
+	{
+		zerabuffer(buf, i);//tira o \n fora
+	}
+	return (1);
+}
+static void strfree(char **s) //ENTENDER PQ EXISTE A CONDICAO s != NULL no if)
+{
+	if (s != NULL && *s != NULL) //(*s != NULL || s != NULL)
+	{
+		free(*s);
+		*s = NULL;
+	}
+}
+static int strinit(char **s, int size) //REDUNDANTE?
+{
+	//if (*s)
+	//	strfree(s); //JA ESTAVA INICIALIZADA
+	//if (!(*s)) //*s == NULL
+	//{
+		if (!(*s = malloc(size * sizeof(char)))) 
+			return (1); //erro Malloc. Retorna NULL pointer
+		(*s)[size - 1] = '\0';
+		zerabuffer(s, 0);
+		return (0);
+	//}
+}
+int gnl(int fd, char **line)
+{
+	ssize_t ret;
+	static char *residual[5]; //AINDA N VOU MEXER C OPEN_MAX
+	//static int count;******************************************
+	char **buf;
+	//char **rest;
+	int oneline;
+	char buffer[BUFF_SIZE + 1]; //TESTE
+    //char buffer[24] = "oi\nmenina\ntudo\nbem?"; //TESTE
+	
+	/* inicializacoes 
+
+	if (count == 0)
+	{
+		buffer[0] = 'o';
+		buffer[1] = 'l';
+		buffer[2] = 'a';
+		buffer[3] = '\n';
+		buffer[4] = 't';
+		buffer[5] = '
+		*/
+	buffer[BUFF_SIZE] = '\0';
+	oneline = 0;
+	//if (!(*line)) //NAO EXISTE LINHA --------------NAO PRECISA TESTAR. TEORICAMENTE N EXISTE!
+		if (strinit(line, 1) < 0) //Malloc na qtidade necessaria
+			return (-1); //erro na MAlloc
+	if (!residual[fd]) //NAO EXISTE RESIDUO PARA ESTE FD
+		if (strinit(&(residual[fd]), (BUFF_SIZE + 1)) < 0) //Malloc na qtidade necessaria
+			return (-1); //erro na MAlloc
+	if (!(buf = malloc(sizeof(char *))))
+		return (-1);
+	*buf = buffer;
+	zerabuffer(buf, 0);
+
+	//if (!(*buf = malloc(BUFF_SIZE*sizeof(char ))))
+	//	return (-1);
+
+
+	/* logica */
+	if (residual[fd])
+	{ //ja estava sendo trabalhado o residual[fd]
+		while (*(residual[fd])) //!= NULL, i.e, existe resto!
+		{
+			strcopy(buf, &(residual[fd]), 0);
+			zerabuffer(&(residual[fd]), 0);
+			oneline = splitbuffer(buf, &(residual[fd]));
+			if (ft_strappend(line, buf))
+				return (-1);
+			if (oneline)
+				return (1);
+		}
+	}
+	//NAo tem residuo!
+	while ((ret = read(fd, *buf, BUFF_SIZE) > 0))
+	{
+		/* desenvolver */
+		oneline = splitbuffer(buf, &(residual[fd]));
+		if (ft_strappend(line, buf))
+			return (-1);
+		if (oneline)
+			return (1);
+	}
+	//ret = 0 -> EOF!
+	oneline = splitbuffer(buf, &(residual[fd]));
+	if (ft_strappend(line, buf))
+		return (-1);
+	//if (oneline)
+	//	return (1);
+	return (0); //EOL ************************************ e se EOL e Uma linha? O que devolve?
 }
 int main()
 {
-	char **s_main;
-	char **buf;
-    char buffer[24] = "oi\nmenina\ntudo\nbem?";
-	char **final;
+	char **linemain;
+	int fd;
+	int gnlret;
+	int count;
 
-	printf("----------------------------\n");
-	printf("Conteudo buffer: 'oi\\nmenina,\\ntudo\\nbem?'\n");
-	s_main = malloc(1*sizeof(char*)); //pointer to pointer
-	buf = malloc(1*sizeof(char*)); //pointer to pointer
-	*buf = buffer; //pointer to string
-	ft_strappend(s_main, buf);//send pointers to pointers
-	final = s_main;
-	printf("LINHA: %s\n", *final);
-	printf("----------------------------\n");
-	return 0;
+	fd = open("42", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("File Open Error\n");
+		return (1);
+	}
+	
+	//fd = 3; //TIRARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+	if (!(linemain = (char **)malloc(1*sizeof(char*)))) //pointer to pointer
+			return (1);
+	while ((gnlret = gnl(fd, linemain)))
+	{
+		if (gnlret < 0)
+		{
+			printf("An error happened in get_next_line!");
+			return (1);
+		}
+		printf("Linha %d: %s\n", count, *linemain);
+		count++;
+		free(*linemain); //Alocou dentro da GNL
+	}
+	//residualFDfree(fd); ??????????????? COMO LIBERA O RESIDUO?
+	if (close(fd) == -1)
+	{
+		printf("File Close Error!\n");
+		//residualFDfree(fd);
+		return (1);
+	}
+	return (0);
 }
